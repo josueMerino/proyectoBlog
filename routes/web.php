f97db04ce49f1b48ca8afd5e37ab180edc8553b3
josueMerino <josue.merino@alumnos.uneatlantico.es>
2020-04-07 12:29:24 +0200

    Los post se muestran en las vistasget_excerpt

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@posts');
+Route::get('blog/{post}', 'PageController@post')->name('post');
+
 
 Auth::routes();
 
