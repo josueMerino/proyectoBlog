c5bae639464631021ad16bbacd0b7fecba2238e9
josueMerino <josue.merino@alumnos.uneatlantico.es>
2020-04-08 14:06:56 +0200

    Ahora se pueden editar y eliminar los artículos con sus imagenes incluidas

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Post;
 use App\Http\Requests\PostRequest;
 
+use Illuminate\Support\Facades\Storage;
 class PostController extends Controller
 {
     /**
@@ -38,7 +39,7 @@ class PostController extends Controller
      */
     public function store(PostRequest $request)
     {
-        dd($request->all());
+        //dd($request->all());
         //Guardar
         $post = Post::create([
             'user_id' => auth()->user()->id
@@ -55,17 +56,6 @@ class PostController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Post  $post
@@ -73,7 +63,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('posts.edit', compact('post'));
     }
 
     /**
@@ -85,7 +75,17 @@ class PostController extends Controller
      */
     public function update(PostRequest $request, Post $post)
     {
-        //
+        //dd($request->all());
+        $post->update($request->all());
+
+        if ($request->file('file')) {
+            //eliminar primero imagen
+            Storage::disk('public')->delete($post->image);
+            $post->image = $request->file('file')->store('posts', 'public');
+            $post->save();
+        }
+
+        return back()->with('status', 'Actualizado con éxito');
     }
 
     /**
@@ -96,6 +96,10 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        //Eliminar imagen
+        Storage::disk('public')->delete($post->image);
+        $post->delete();
+
+        return back()->with('status', 'Eliminado con éxito');
     }
 }
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Editar Artículo</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form action="{{ route('posts.update', $post) }}" method="post" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label>Título *</label>
+                            <input type="text" name="title" class="form-control" required value="{{old('title', $post->title)}}">
+                        </div>
+                        <div class="form-group">
+                            <label>Imagen</label>
+                            <input type="file" name="file">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Contenido *</label>
+                            <textarea name="body" rows="6" class="form-control" required>"{{old('body', $post->body)}}" ></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Contenido embebido</label>
+                            <textarea name="iframe"  class="form-control" {{old('iframe', $post->iframe)}} ></textarea>
+                        </div>
+                        <div class="form-group">
+                            @csrf
+                            @method('put')
+                            <input type="submit" value="Actualizar" class="btn btn-sm btn-primary">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
