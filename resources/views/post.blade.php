a9c7ab358f75b24c33ea1fa42c474fbd703c06c6
josueMerino <josue.merino@alumnos.uneatlantico.es>
2020-04-20 12:13:54 +0200

    Modificacion visual

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,12 +9,12 @@
                     @if ($post->image)
                         <img src="{{$post->get_image}}" class="card-img-top">
                     @endif
+                    <h5 class="card-title">{{$post->title}}</h5>
                     @if($post->iframe)
                         <div class="embed-responsive embed-responsive-16by9">
                             {!! $post->iframe !!}
                         </div>
                     @endif
-                    <h5 class="card-title">{{$post->title}}</h5>
                     <p class="card-text">
                         {{$post->body}}
                     </p>
